12b5ed76676f70151be893cda05408ac97610f23
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-08-09 14:54:45 -0500

    Change function name and Added Comments
    -Changed Function Name on line 38 to better communicate the function it is preforming
    -Added Comments to the project :)

--- employee_directory_v1/scripts.js
+++ employee_directory_v1/scripts.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
 
 
-
-
+    // Gets all the information from the AJAX request and sends it to the paige
     var getEmployees = (function () {
+
+        // Preform 12 AJAX requests to get 12 Employees on the page
         for (var i = 0; i < 12; i++) {
+            //The url for the API used in this project
             var randUserAPI = 'https://randomuser.me/api/';
 
+            //Displays the different facets of the JSON data to the page
             function displayInformation(data) {
+
+                //Created an initial variable to build off of for each portion of the employee div
                 var employeeHTML = '<div class="employee"><div class="profileImage">';
                 employeeHTML += '<img src="' + data.results[0].picture.large + '" alt="Employee Photo">';
                 employeeHTML += '</div><div class="profileInformation">';
@@ -22,40 +27,51 @@ $(document).ready(function () {
 
             }
 
-
+            //jQuery JSON request 
             $.getJSON(randUserAPI, displayInformation);
 
         }
 
     })(); // end getEmployees
 
-    var displayName = (function () {
+
+    var displayNameModal = (function () {
         // When the user clicks the button, open the modal
         $(".employees").on("click", ".employee", function () {
 
+            // Finds the image from the element to be displayed in the modal
             var thisImage = $(this).find("img").attr("src");
             $('.spanPhoto').attr("src", thisImage);
 
+            // References the opject for the displayText function
             var thisObject = $(this);
 
+            // Displays text from a given element to the modal
             function displayText(spanName, elementName) {
                 $(spanName).text($(thisObject).find(elementName).text());
             }
 
+            //Displays the full name in the modal
             displayText('.spanFullName', ".fullName");
 
+            //Displays the email in the modal
             displayText('.spanEmail', ".email");
 
+            //Displays the location in the modal
             displayText('.spanLocation', ".location");
 
+            //Displays the phone number in the modal
             displayText('.spanPhoneNumber', ".phoneNumber");
 
+            //Displays the full address in the modal
             displayText('.spanFullAddress', ".address");
 
+            //Because I wanted an additional piece of code in the birthday
+            // text this would not be refactored with the above code/
             var thisBirthday = $(this).find(".birthday").text();
             $('.spanBirthday').text("Birthday: " + thisBirthday);
 
-
+            // Displays the complete modal with all information
             $('#myModal').css("display", "block");
         });
 
@@ -63,7 +79,7 @@ $(document).ready(function () {
         $(".modal-content").on("click", ".close", function () {
             $('#myModal').css("display", "none");
         });
-    })();
+    })(); // end displayNameModal
 
 
 
